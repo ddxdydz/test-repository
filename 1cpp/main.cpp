aa50aa9304c49b402a22df2c82db083d9f3a261b
zz <asdfgtrewq2485@mail.ru>
2026-03-27 14:39:51 +0700

    gsd

--- 1cpp/main.cpp
+++ 1cpp/main.cpp
@@ -22,7 +22,7 @@ void initGrayLUT() {
     for (int r = 0; r < 256; r++) {
         for (int g = 0; g < 256; g++) {
             for (int b = 0; b < 256; b++) {
-                gray_lut[(r << 16) | (g << 8) | b] = (r * 77 + g * 150 + b * 29) >> 13;
+                gray_lut[(r << 16) | (g << 8) | b] = (r * 299 + g * 587 + b * 114) >> 10;
             }
         }
     }
